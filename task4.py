a8f260244789ddfd85c4a8a06de479be72198a68
Anna Vernyaeva <ann.vernyaeva@gmail.com>
2022-10-06 12:34:55 +0300

    Initial commit

---
+++ task4.py
@@ -0,0 +1,20 @@
+# 4 задача. Реализуйте алгоритм перемешивания списка.
+
+# import random
+# list = [1, 34, 23, 54, 7, 9]
+# random.shuffle(list)
+
+# print(list)
+
+my_list = [1, 2, 3, 7, 98]
+length = len(my_list)
+
+for i in range(0, length):
+    for j in range(1, length - i):
+        temp = my_list[i]
+        my_list[i] = my_list[j]
+        my_list[j] = temp
+        i += 1
+        j += 1
+print(my_list)
+
